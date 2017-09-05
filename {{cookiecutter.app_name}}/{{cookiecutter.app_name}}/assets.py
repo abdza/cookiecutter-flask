f7872e64f5b859b7450f48453addfb979a0c9a93
abdza <abdullah.zainul@gmail.com>
2017-09-05 15:11:15 +0800

    Added adminLTE theme

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/assets.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/assets.py
@@ -4,8 +4,11 @@ from flask_assets import Bundle, Environment
 
 css = Bundle(
     'libs/bootstrap/dist/css/bootstrap.css',
+    'libs/bootstrap/dist/css/bootstrap-datepicker.css',
     'libs/font-awesome4/css/font-awesome.min.css',
     'libs/material-design/material-icons.css',
+    'libs/adminLTE/css/AdminLTE.css',
+    'libs/adminLTE/css/skins/skin-yellow.css',
     'css/style.css',
     filters='cssmin',
     output='public/css/common.css'
@@ -14,6 +17,8 @@ css = Bundle(
 js = Bundle(
     'libs/jQuery/dist/jquery.js',
     'libs/bootstrap/dist/js/bootstrap.js',
+    'libs/adminLTE/js/adminlte.js',
+    'libs/adminLTE/js/app.js',
     'js/plugins.js',
     filters='jsmin',
     output='public/js/common.js'
